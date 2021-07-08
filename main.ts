6c3706bce027273ce8308d9cfcbfa0aee4a8fd0e
qch3173 <86691137+qch3173@users.noreply.github.com>
2021-07-08 12:24:37 +1200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+ 
+for(let i = 1; i < 1000; i++) {
+      basic.showLeds(`
+ # # # # #
+ . # # # .
+ . # # # .
+ . # # # .
+ . # . # .
+ `,10) 
+ basic.showLeds(`
+ . # . # .
+ # # # # #
+ . # # # .
+ . # # # .
+ . # # # .
+ `,10)   
+ basic.showLeds(`
+ . # . # .
+ . # . # .
+ # # # # #
+ . # # # . 
+ . # # # .`,10)
+  basic.showLeds(`
+ . # . # .
+ . # . # .
+ . # . # .
+ # # # # # 
+ . # # # .`,10)
+    basic.showLeds(`
+ . # . # .
+ . # . # .
+ . # . # .
+ . # . # . 
+ # # # # #`,10)
+
+}
+ 
+
+
+})
+
+radio.setGroup(222)
+input.onButtonPressed(Button.B, function () {
+ radio.sendString("I like little bois")   
 })
